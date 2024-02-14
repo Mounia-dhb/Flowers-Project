b312ede5c0226b3a04b90b0e3ae57ba432e353a9
Tjr-ds <hatim.touijar20@gmail.com>
2024-02-14 10:26:05 +0100

    minor changes

--- store/models.py
+++ store/models.py
@@ -41,7 +41,6 @@ class Category(models.Model):
 	def __str__(self):
 		return self.name
 
-	#@daverobb2011
 	class Meta:
 		verbose_name_plural = 'categories'
 
@@ -65,7 +64,7 @@ class Product(models.Model):
 	name = models.CharField(max_length=100)
 	price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
 	category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
-	description = models.CharField(max_length=250, default='', blank=True, null=True)
+	description = models.CharField(max_length=1000, default='', blank=True, null=True)
 	image = models.ImageField(upload_to='uploads/product/')
 	# Add Sale Stuff
 	is_sale = models.BooleanField(default=False)
