7ada87e0293b8ecad185a999816ad9c08250e327
Tjr-ds <hatim.touijar20@gmail.com>
2024-02-23 20:46:48 +0100

    fixed paypal

--- ecom/settings.py
+++ ecom/settings.py
@@ -130,3 +130,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# PayPal settings
+
+PAYPAL_TEST = True
+
+PAYPAL_RECEIVER_EMAIL = 'sb-n43zgy29628768@business.example.com'
