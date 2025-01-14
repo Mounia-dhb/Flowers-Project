fdeea07e503bdd0862f9c39730ee19f5b2aff048
souhamora <159634441+souhamora@users.noreply.github.com>
2025-01-14 12:33:01 +0100

    last version

--- ecom/views.py
+++ ecom/views.py
@@ -5,5 +5,5 @@ def payment_completed_view(request):
     return HttpResponse("Payment completed successfully!")
 
 def payment_failed_view(request):
-    return HttpResponse("Payment failed!")
+    return HttpResponse("!")
 
